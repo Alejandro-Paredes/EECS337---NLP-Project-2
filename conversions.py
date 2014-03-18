81bfc8e3a3010142e9799f85e7c71e6b79802364
Cathy Jo Lopez <cathylopez2015@u.northwestern.edu>
2014-03-18 17:09:43 -0500

    conversions and dairy subsitutes

---
+++ conversions.py
@@ -0,0 +1,202 @@
+def convertTeaspoonToCup(teaspoon):
+	cup = round(teaspoon*0.021, 2)
+	return cup
+	
+def convertCupToTeaspoon(cup):
+	teaspoon = round(cup/0.021, 2)
+	return teaspoon
+
+def convertTeaspoonToTablespoon(teaspoon):
+	tablesoon = round(teaspoon/3, 2)
+	return tablespoon
+
+def convertTablespoonToTeaspoon(tablespoon):
+	teaspoon = round(tablespoon*3, 2)
+	return teaspoon
+	
+def convertTeaspoonToOz(teaspoon):
+	oz = round(teaspoon*0.167, 2)
+	return oz
+
+def convertOztoTeaspoon(oz):
+	teaspoon = round(oz/0.167, 2)
+	return teaspoon
+	
+def convertTeaspoonToMl(teaspoon):
+	ml = round(teaspoon*5, 2)
+	return ml
+
+def convertMltoTeaspoon(ml):
+	teaspoon = round(ml/5, 2)
+	return teaspoon
+	
+def convertTablespoonToOz(tablespoon):
+	oz = round(tablespoon*0.5, 2)
+	return oz
+	
+def convertOzToTablespoon(oz):
+	tablespoon = round(oz*2, 2)
+	return tablespoon
+	
+def convertTablespoonToGram(tablespoon):
+	gram = round(tablespoon*14, 2)
+	return gram
+
+def convertGramToTablespoon(gram):
+	tablespoon = round(gram/14, 2)
+	return tablespoon
+
+def convertTablespoonToMl(tablespoon):
+	ml = round(tablespoon*14.786, 2)
+	return ml
+
+def convertMlToTablepoon(ml):
+	tablespoon = round(ml/14.786, 2)
+	return tablespoon
+
+def convertTablespoonToCup(tablespoon):
+	cup = round(tablespoon*0.0625, 2)
+	return cup
+	
+def convertCupToTablespoon(cup):
+	tablespoon = round(cup/0.0625, 2)
+	return tablespoon
+
+def convertTablespoonToLb(tablespoon):
+	lb = round(
+	return lb
+	
+def convertLbToTablespoon(lb):
+	tablespoon = round(
+	return tablespoon
+	
+def convertTablespoonToCup(tablespoon):
+	cup = round(
+	return cup
+	
+def convertCupToTablespoon(cup):
+	tablespoon = round(
+	return tablespoon
+	
+def convertTablespoonToPint(tablespoon):
+	pint = round(tablespoon*0.031, 2)
+	return pint
+	
+def convertCupToOz(cup):
+    oz = round(cup*8.0, 2)
+    return oz
+
+def convertOzToCup(oz):
+    cup = round(oz/8.0, 2)
+    return cup
+    
+def convertCupToGallon(cup):
+	gallon = round(cup*0.0625, 2)
+	return gallon
+	
+def convertGallonToCup(gallon):
+	cup = round(gallon/0.0625, 2)
+	return cup
+	
+def convertCupToLiter(cup):
+	l = round(cup*0.25, 2)
+	return l
+	
+def convertLiterToCup(l):
+	cup = round(l*4, 2)
+	return cup
+	
+def convertCupToPint(cup):
+	pint = round(cup*0.5, 2)
+	return pint
+	
+def convertPintToCup(pint):
+	cup = round(pint*2, 2)
+	return cup
+	
+def convertCuptoQt(cup):
+	qt = round(cup*0.25, 2)
+	return qt
+
+def convertQtToCup(qt):
+	cup = round(qt*4, 2)
+	return cup
+	
+def convertOzToLiter(oz):
+	liter = round(oz*0.03, 2)
+	return liter
+	
+def convertLiterToOz(liter):
+	oz = round(liter/0.03, 2)
+    return oz
+    
+def convertOzToQt(oz):
+	qt = round(oz*0.031, 2)
+	return qt
+	
+def convertQtToOz(qt):
+	oz = round(qt/0.031, 2)
+	return oz
+	
+def convertOzToPint(oz):
+	pint = round(oz/16, 2)
+	return pint
+	
+def convert PintToOz(pint):
+	oz = round(pint*16, 2)
+	return oz
+	
+def convertGallonToLiter(gallon):
+	liter = round(gallon*3.785, 2)
+	return liter
+
+def convertLiterToGallon(liter):
+	gallon = round(liter/3.785, 2)
+	return gallong
+
+def convertGallonToOz(gallon):
+	oz = round(gallon*128, 2)
+	return oz
+	
+def convertOzToGallon(oz):
+	gallon = round(oz/128, 2)
+	return gallon
+
+def convertGallonToPint(gallon):
+	pint = round(gallon*8, 2)
+	return pint
+
+def convertPintToGallon(pint):
+	gallon = round(pint/8, 2)
+	return gallon
+
+def convertGallonToQt(gallon):
+	qt = round(gallon*4, 2)
+
+def convertQtToGallon(qt):
+	gallon = round(qt/4, 2)
+	return gallon
+
+def convertOzToLb(oz):
+	lb = round(oz/16, 2)
+	return lb
+
+def convertLbToOz(lb):
+	oz = round(lb*16, 2)
+	retun oz
+
+def convertOzToKg(oz):
+	kg = round(oz/35.272, 2)
+	return kg
+
+def convertKgToOz(kg):
+	oz = round(kg*35.273, 2)
+	retun oz
+	
+def convertLbToKg(lb):
+	kg = round(lb*0.4375, 2)
+	return kg
+	
+def convertKgTolb(kg):
+	lb = round(kg/0.4375, 2)
+	return lb
